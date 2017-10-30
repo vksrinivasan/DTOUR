1da46a7e3d5cc51e354290a75280e8a58c58d07a
Shinya Hirata <shirata@gatech.edu>
2017-10-30 02:12:17 -0400

    Transition graphs with custom quantization and sql

--- backend/graph_generation/transition/generate_transition_graph.py
+++ backend/graph_generation/transition/generate_transition_graph.py
@@ -11,6 +11,9 @@ import numpy as np
 import json
 import os
 import errno
+import math
+import sqlite3
+import re
 
 # Outline
 # main function: reads csv
@@ -19,26 +22,46 @@ import errno
 # Add that to kxk matrix
 # Once we're done calculate transition matrix and write to output.json
 
+NEAREST_NODE_TOLERANCE = 0.5 # If dest or origin is greater than this distance in km, throw trip away
 
-NUM_NODES = 265
 
+def generate_trip_graph(data_file, quantizer, interval_start, interval_end):
+    trip_graph = np.zeros((quantizer.num_nodes(), quantizer.num_nodes()), dtype=np.int)
 
-def generate_trip_graph(interval_start, interval_end, data_path):
-    trip_graph = np.zeros((NUM_NODES, NUM_NODES), dtype=np.int)
-
-    with open(data_path) as f:
+    with open(data_file) as f:
         rows = (row for row in csv.reader(f) if len(row) > 0)
         header = rows.next()
-        pu_id_index = header.index('PULocationID')
-        do_id_index = header.index('DOLocationID')
-        pu_time_index = header.index('tpep_pickup_datetime')
+
+        header = [column.strip() for column in header]
+        pu_lat_index = header.index('pickup_latitude')
+        pu_long_index = header.index('pickup_longitude')
+        do_lat_index = header.index('dropoff_latitude')
+        do_long_index = header.index('dropoff_longitude')
+        for i, column in enumerate(header):
+            if 'pickup_datetime' in column:
+                pu_time_index = i
+                break
 
         for row in rows:
             pu_date = dateutil.parser.parse(row[pu_time_index])
             if pu_date.time() >= interval_start and pu_date.time() < interval_end:
-                pu_id = int(row[pu_id_index])
-                do_id = int(row[do_id_index])
-                trip_graph[pu_id-1, do_id-1] += 1
+                try:
+                    pu_lat = float(row[pu_lat_index])
+                    pu_long = float(row[pu_long_index])
+                    do_lat = float(row[do_lat_index])
+                    do_long = float(row[do_long_index])
+
+                    pu_node, pu_dist = quantizer.query((pu_lat, pu_long))
+                    do_node, do_dist = quantizer.query((do_lat, do_long))
+
+                    pu_dist = pu_dist * 6371 * math.pi /180  # Angle to km
+                    do_dist = do_dist * 6371 * math.pi / 180  # Angle to km
+
+                    if pu_dist <= NEAREST_NODE_TOLERANCE and do_dist <= NEAREST_NODE_TOLERANCE:
+                        trip_graph[pu_node.index, do_node.index] += 1
+                except ValueError:
+                    continue
+
 
     return trip_graph
 
@@ -53,34 +76,71 @@ if __name__ == '__main__':
     else:
         interval_end = dt.time(hour=int(hour2), minute=int(minute2))
 
-    data_dir = sys.argv[3]
-    data_paths = [os.path.join(data_dir, data_file) for data_file in os.listdir(data_dir)]
-    data_paths = [data_path for data_path in data_paths if os.path.isfile(data_path)]
+    data_path = sys.argv[3]
+    if os.path.isfile(data_path):
+        data_files = [data_path]
+    elif os.path.isdir(data_path):
+        data_files = [os.path.join(data_path, data_file_name) for data_file_name in os.listdir(data_path)]
+        data_files = [data_file for data_file in data_files if os.path.isfile(data_file)]
+    else:
+        raise ValueError('{} is not a valid file path'.format(data_path))
 
-    output_dir = sys.argv[4]
+    graph_db = sys.argv[4]
+    quantizer = Quantizer(graph_db)
 
-    for data_path in data_paths:
-        trip_graph = generate_trip_graph(interval_start, interval_end, data_path)
-        outbound_degrees = trip_graph.sum(axis = 1)
-        transition_probabilities = trip_graph.astype(np.float) / outbound_degrees[:, np.newaxis]
-        transition_probabilities = np.nan_to_num(transition_probabilities) # Replace nans with 0's
+    for data_file in data_files:
+        trip_graph = generate_trip_graph(data_file, quantizer, interval_start, interval_end)
+        # outbound_degrees = trip_graph.sum(axis = 1)
+        # transition_probabilities = trip_graph.astype(np.float) / outbound_degrees[:, np.newaxis]
+        # transition_probabilities = np.nan_to_num(transition_probabilities) # Replace nans with 0's
 
-        data_file = os.path.split(data_path)[1]
-        data_file_output_dir = os.path.join(output_dir, os.path.splitext(data_file)[0])
+        data_file_name = os.path.split(data_file)[1]
+
         try:
-            os.mkdir(data_file_output_dir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise e
+            match = re.search(r'-(\d\d)(?!d)', data_file_name)
+            month = int(match.group(1))
+        except (ValueError, AttributeError):
+            month = 0
+            sys.stderr.write('Could not parse month from {}\n'.format(data_file_name))
 
-        output_path = os.path.join(data_file_output_dir, '{:02d}{:02d}-{:02d}{:02d}.json'
-                                   .format(int(hour1), int(minute1), int(hour2), int(minute2)))
-        with open(output_path, 'w+') as f:
-            results = {}
-            results['transition_graph'] = transition_probabilities.tolist()
-            results['num_pickups'] = outbound_degrees.tolist()
-            results['data_files'] = [os.path.split(data_path)[1] for data_path in data_paths]
-            results['interval'] = [interval_start.isoformat(), interval_end.isoformat()]
-            results['num_zones'] = NUM_NODES
+        try:
+            match = re.search(r'^(.+?)_', data_file_name)
+            taxi_type = match.group(1)
+        except AttributeError:
+            taxi_type = None
+            sys.stderr.write('Could not parse taxi type from {}\n'.format(data_file_name))
 
-            json.dump(results, f, sort_keys=True)
+        interval_start = int(hour1)*60 + int(minute1)
+        interval_end = int(hour2)*60 + int(minute2)
+        graph_name = '{}_{}_{}'.format(os.path.splitext(data_file_name)[0], interval_start, interval_end)
+
+        transition_graph_row = (graph_name, month, taxi_type, interval_start, interval_end)
+        with sqlite3.connect(graph_db) as conn:
+            conn.execute('PRAGMA foreign_keys = ON')
+            conn.execute('INSERT INTO transition_graphs (name, month, taxi_type, '
+                         'interval_start, interval_end) '
+                         'values (?,?,?,?,?)', transition_graph_row)
+
+            cursor = conn.execute('SELECT id from transition_graphs WHERE name=\'{}\''.format(graph_name))
+            transition_graph_id = next(cursor)[0]
+
+            conn.commit()
+
+        # list of (transition_graph_id, source_node_id, dest_node_id, weight)
+        l_edges = []
+
+        for source_index in range(quantizer.num_nodes()):
+            for dest_index in range(quantizer.num_nodes()):
+                weight = trip_graph[source_index, dest_index]
+                src_node_id = quantizer.nodes[source_index].db_id
+                dest_node_id = quantizer.nodes[dest_index].db_id
+                if weight > 0:
+                    l_edges.append((transition_graph_id, src_node_id, dest_node_id, weight))
+
+        with sqlite3.connect(graph_db) as conn:
+            conn.execute('PRAGMA foreign_keys = ON')
+            conn.executemany('INSERT INTO transition_edges (transition_graph_id, '
+                         'source_node_id, dest_node_id, weight)'
+                         'values (?,?,?,?)', l_edges)
+
+            conn.commit()
--- backend/graph_generation/transition/quantization.py
+++ backend/graph_generation/transition/quantization.py
@@ -1,15 +1,71 @@
+from scipy import spatial
+import numpy as np
+import sqlite3
+import collections
+
 class Quantizer():
-    def __init__(self, file_name):
-        self.__load_file(file_name)
+    def __init__(self, db_file_name):
+        self.__load_file(db_file_name)
+        self.__build_tree()
 
 
-    def __load_file(self, file_name):
+    def __load_file(self, db_file_name):
+        with sqlite3.connect(db_file_name) as conn:
+            cursor = conn.execute("SELECT id, name, latitude, longitude from nodes ORDER by id ASC")
+            self.nodes = [Node(*row, index=i) for i, row in enumerate(cursor)]
+            cursor.close()
+
+            conn.commit()
+
+
+    def __build_tree(self):
+        coords = np.array([(node.lat, node.long) for node in self.nodes])
+        # self.coord_map = collections.defaultdict(list)
+        # for i, coord in enumerate(coords):
+        #     self.coord_map[coord[0], coord[1]] = self.nodes[i]
+
+        self._kdtree = spatial.KDTree(coords)
         pass
 
 
-    def query(self, point):
+    def query(self, lat, long, k=1):
+        result = self.query((lat, long), k)
+        return result[0]
+
+
+    def query(self, points, k=1):
         """
-        Returns the quantized bin
-        :type point: (latitude, longitude)
+        Returns the nearest node
+        :param points: tuple(lat,long) or list of tuples
+        :returns tuples of (Node, dist) in same dimensionality as points was specified
         """
-        pass
+        dists, indexes = self._kdtree.query(points, k=k)
+        return self.__to_nodes(indexes, dists)
+
+
+    def __to_nodes(self, indexes, dists):
+        try:
+            return [self.__to_nodes(index, dist) for index, dist in zip(indexes, dists)]
+        except (TypeError, ValueError): # not iterable
+            return self.nodes[indexes], dists
+
+
+    def num_nodes(self):
+        return len(self.nodes)
+
+
+class Node():
+    def __init__(self, db_id, name, lat, long, index):
+        '''
+
+        :param db_id: database id
+        :param name:
+        :param lat:
+        :param long:
+        :param index: used to number nodes from 0 to nodes.length-1
+        '''
+        self.db_id = db_id
+        self.name = name
+        self.lat = np.float(lat)
+        self.long = np.float(long)
+        self.index = index
