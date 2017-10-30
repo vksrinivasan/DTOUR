490898899393160aea597de2f34916fc7989024a
Shinya Hirata <shirata@gatech.edu>
2017-10-30 03:30:38 -0400

    Modify shell script for graph generation

--- backend/graph_generation/generate_graphs.sh
+++ backend/graph_generation/generate_graphs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 LOG_DIR='logs'
 DB_FILE='../../graph/graph_database.db'
-
+DATA_DIR='../../data/nyc_gov_trip_data'
+BASE_URL='https://s3.amazonaws.com/nyc-tlc/trip+data'
+DOWNLOAD_DIR='../../data/nyc_gov_trip_data'
 
 cd "$(dirname "$0")"
 
@@ -11,7 +13,26 @@ if [ ! -f $DB_FILE ]; then
 fi
 
 mkdir -p $LOG_DIR
-for i in {0..23}
+for year in {2014..2015}
 do
-    python transition/generate_transition_graph.py "$i:00" "`expr $i + 1`:00" ../../data/nyc_gov_trip_data/ ../../graph 2> $LOG_DIR"/"$i"00-`expr $i + 1`00.txt"
+	for month in `seq -f "%02g" 1 12`
+	do
+		DATA_NAME="yellow_tripdata_$year-$month"
+		LOG_DATA_DIR="$LOG_DIR/$DATA_NAME"
+		mkdir -p $LOG_DATA_DIR
+
+		DOWNLOAD_URL="$BASE_URL/$DATA_NAME.csv"
+		DOWNLOAD_FILE="$DOWNLOAD_DIR/$DATA_NAME.csv"
+		
+		# echo $DOWNLOAD_URL
+		# echo $DOWNLOAD_FILE
+
+		wget -O $DOWNLOAD_FILE $DOWNLOAD_URL
+		for hour in `seq -f "%02g" 0 23`
+		do
+		    python -W ignore transition/generate_transition_graph.py "$hour:00" "`expr $hour + 1`:00" $DOWNLOAD_FILE $DB_FILE 2> $LOG_DATA_DIR"/"$hour"00-`expr $hour + 1`00.txt"
+		done
+		rm $DOWNLOAD_FILE
+	done
 done
+
