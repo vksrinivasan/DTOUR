67d0a80551815cebe38d4d45c05ec10e4af2690d
vksrinivasan <ndnkai@gmail.com>
2017-10-31 13:56:14 -0400

    Edited AStar to be able to handle intermediate sidetrack edges. Also, no longer runs until completion - now iteratively runs until it finds/updates target (see Main.java)

--- backend/Main.java
+++ backend/Main.java
@@ -6,7 +6,12 @@ public class Main {
         Heuristic myHeuristic = new NullHeuristic_Implement();
         PathGraph myPathGraph = new PathGraph();
         AStar myAlgo = new AStar(0,3, mySource, myHeuristic, myPathGraph);
-        myAlgo.runAlgo();
+
+        // Currently the A* code stops running once it finds a path to the destination - I have this while loop here
+        // to keep running it until it completes (finds shortest/most probabilistic path to destination), but we can
+        // remove this loop and just run it once. Or we could try to implement the feedback scheduling from the paper.
+        while(!myAlgo.searchComplete)
+            myAlgo.runAlgo();
         List<Integer> order = myAlgo.reconstructPath();
 
         for(Integer e : order) { System.out.println(e.toString()); };
