b463b3208dee35af70621a30957f1914d7cf81fd
vksrinivasan <ndnkai@gmail.com>
2017-11-05 19:30:37 -0500

    Finished!

--- DTOURCode/src/main/java/Main.java
+++ DTOURCode/src/main/java/Main.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int start_id = 1070;
-        int end_id = 1023;
+        int start_id = 659;
+        int end_id = 660;
 
         DataSource mySource = new MySqlDataSource();
         mySource.initSource();
@@ -24,9 +24,6 @@ public class Main {
         Graph g=new Graph();
         
         HashMap <Integer,MinHeap> HtCollection=Ht.createHtCollection(order, myPathGraph.Hin_Collection, g);
-        
-        //myPathGraph.treeHeapCollection = new HashMap<Integer,Heap_In>(myPathGraph.Hin_Collection);
-       //myPathGraph.treeHeapCollection=new HashMap(myPathGraph.Hin_Collection);
         
         int dest=order.get(order.size()-1);
         System.out.println(HtCollection.get(dest));
@@ -47,50 +44,3 @@ public class Main {
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-/*import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-
-        DataSource mySource = new StaticData_Implement();
-        mySource.initSource();
-        double []NodeLongLat = mySource.getGeoList(); // We will keep longitudes/latitudes in memory (only ~1000 nodes)
-        Heuristic myHeuristic = new DistanceHeuristic();
-        PathGraph myPathGraph = new PathGraph();
-        AStar myAlgo = new AStar(0,6, mySource, myHeuristic, myPathGraph, NodeLongLat);
-
-        // Time it and keep track of A* iteration
-        final long startTime = System.currentTimeMillis();
-        int iteration = 0;
-
-        // Currently the A* code stops running once it finds a path to the destination - I have this while loop here
-        // to keep running it until it completes (finds shortest/most probabilistic path to destination), but we can
-        // remove this loop and just run it once. Or we could try to implement the feedback scheduling from the paper.
-        while(!myAlgo.searchComplete) {
-            System.out.println("Curr Iteration: " + iteration);
-            myAlgo.runAlgo();
-            iteration++;
-        }
-        List<Integer> order = myAlgo.reconstructPath();
-
-        // Finish timing
-        final long endTime = System.currentTimeMillis();
-
-        for(Integer e : order) { System.out.println(e.toString()); };
-        System.out.println("");
-        System.out.println(endTime - startTime);
-        mySource.closeSource();
-    }
-}
-
-*/
