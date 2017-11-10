1ac0ac65127517d9a3f8704dc59e3a5b87416763
Shinya Hirata <shirata@gatech.edu>
2017-11-10 00:18:56 -0500

    Add functionality to run on cluster

--- graph_generation/generate_graphs.sh
+++ graph_generation/generate_graphs.sh
@@ -5,9 +5,16 @@ ROOT_DIR=`realpath $ROOT_DIR`
 LOG_DIR="$ROOT_DIR/graph_generation/logs"
 BASE_URL='https://s3.amazonaws.com/nyc-tlc/trip+data'
 DOWNLOAD_DIR="$ROOT_DIR/data/nyc_gov_trip_data"
+HADOOP_DIR="DTOUR/data/nyc_gov_trip_data"
 INTERVAL_LENGTH=60
 
-export PYTHONPATH=`realpath "$ROOT_DIR/graph_generation"`
+CLUSTERED=false
+if [ "$1" = '-c' ]; then
+	CLUSTERED=true
+	hadoop fs -mkdir -p $HADOOP_DIR
+fi
+
+export PYTHONPATH=`realpath "$ROOT_DIR/graph_generation"`:$PYTHONPATH
 
 credentials=`cat  $ROOT_DIR/graph_generation/credentials.txt |tr "\n" " "`
 host=`echo $credentials | awk '{print $1;}'`
@@ -31,15 +38,38 @@ do
 
 		DOWNLOAD_URL="$BASE_URL/$DATA_NAME.csv"
 		DOWNLOAD_FILE="$DOWNLOAD_DIR/$DATA_NAME.csv"
+		HADOOP_FILE="$HADOOP_DIR/$DATA_NAME.csv"
 		
-		# echo $DOWNLOAD_URL
-		# echo $DOWNLOAD_FILE
 
 		wget -O $DOWNLOAD_FILE $DOWNLOAD_URL
-		python -W ignore $ROOT_DIR/graph_generation/transition/generate_transition_graph.py $INTERVAL_LENGTH $DOWNLOAD_FILE &> $LOG_DATA_FILE
+		if [ $CLUSTERED = 'true' ]; then
+			hadoop fs -put $DOWNLOAD_FILE $HADOOP_FILE
+			# python -W ignore $ROOT_DIR/graph_generation/transition/generate_transition_graph.py -c $INTERVAL_LENGTH $HADOOP_FILE &> $LOG_DATA_FILE
+			spark-submit --master yarn-client --deploy-mode client \
+				--py-files="$ROOT_DIR/graph_generation/transition/quantization.py,$ROOT_DIR/graph_generation/mysql_util.py" \
+				$ROOT_DIR/graph_generation/transition/generate_transition_graph.py \
+				$INTERVAL_LENGTH $HADOOP_FILE &> $LOG_DATA_FILE
+			hadoop fs -rm $HADOOP_FILE
+		else
+			# python -W ignore $ROOT_DIR/graph_generation/transition/generate_transition_graph.py $INTERVAL_LENGTH $DOWNLOAD_FILE &> $LOG_DATA_FILE
+			spark-submit --master local \
+				--py-files="$ROOT_DIR/graph_generation/transition/quantization.py,$ROOT_DIR/graph_generation/mysql_util.py" \
+				$ROOT_DIR/graph_generation/transition/generate_transition_graph.py \
+				$INTERVAL_LENGTH $DOWNLOAD_FILE &> $LOG_DATA_FILE
+		fi
+
 		rm $DOWNLOAD_FILE
 	done
 done
 
 # python -W ignore $ROOT_DIR/graph_generation/transition/generate_transition_graph.py $INTERVAL_LENGTH $ROOT_DIR/data/nyc_gov_trip_data/yellow_tripdata_2016-03_min.csv
 
+# spark-submit --master local \
+# 	--py-files="$ROOT_DIR/graph_generation/transition/quantization.py,$ROOT_DIR/graph_generation/mysql_util.py" \
+# 	$ROOT_DIR/graph_generation/transition/generate_transition_graph.py \
+# 	$INTERVAL_LENGTH $ROOT_DIR/data/nyc_gov_trip_data/yellow_tripdata_2014-01.csv
+
+# spark-submit --master yarn-client --deploy-mode client \
+# 	--py-files="$ROOT_DIR/graph_generation/transition/quantization.py,$ROOT_DIR/graph_generation/mysql_util.py" \
+# 	$ROOT_DIR/graph_generation/transition/generate_transition_graph.py \
+# 	$INTERVAL_LENGTH data/nyc_gov_trip_data/yellow_tripdata_2014-01.csv
